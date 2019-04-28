3c0efe113a310b68dc86389b83b6111cd95e640d
TanujaNadaf <48946686+TanujaNadaf@users.noreply.github.com>
2019-04-28 14:19:38 +0530

    Add files via upload

--- Oops Programs/inventoryDetails.js
+++ Oops Programs/inventoryDetails.js
@@ -1,10 +1,20 @@
-var fs=require('fs');
-var inventoryDetails=fs.readFileSync("inventoryDetails.json");
+/*********************************************************************************************************************************
+ *  @Purpose       : Takes the inventory json file as input,parse to json object,calculate the value of each inventory and display the value
+ *  @author        : Tanuja Nadaf
+ *  @file          : inventoryDetails.js
+ *  @since         : 24/04/2019
+ **********************************************************************************************************************************/
+/* import the file stream module*/
+ var fs=require('fs');
+/* read the file to a variable using file stream object*/
+ var inventoryDetails=fs.readFileSync("inventoryDetails.json");
 console.log(inventoryDetails);
+/* parse to a json object */
 var inventoryObject=JSON.parse(inventoryDetails);
 console.log(inventoryObject);
 var myJSON=JSON.stringify(inventoryObject);
 console.log(myJSON);
+/* calculate the value of each inventory using for loop */
 for(index in inventoryObject.rice)
 {
     console.log("The price of"+" "+inventoryObject.rice[index].riceName +" "+ "for"+" "+inventoryObject.rice[index].weight+ "kgs is" +" "+(inventoryObject.rice[index].weight*inventoryObject.rice[index].price));
